db5aacda9557ebaa2e101e6450f9e374760d0b20
shubhamverma-code <shubhamverma9454354170@gmail.com>
2023-01-18 09:38:21 +0530

    Abstraction Concept

---
+++ Abstraction/Abstraction.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+/*
+Abstraction
+means displaying only essential and hiding the details
+
+e.g calculator, sort() function, car acceleration
+
+
+*/
+int main(){
+    int n = 8;
+    int power = 3;
+   int result = pow(n,power);
+   cout<<"Power is : "<<result<<endl;
+}
